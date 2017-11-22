c52883c5f4b45a9326dbf0fe38ac6c900904ac34
Antonis Stampoulis <antonis.stampoulis@originate.com>
2017-11-22 17:02:32 -0500

    add payload to middleware (WIP)

--- src/index.js
+++ src/index.js
@@ -11,26 +11,30 @@ export type ClientData<I: {}> = {
   queryParams: { [string]: TypeRep<any> }
 };
 
-interface Middleware<I_old: {}, I: {}> {
-  mapServerData(input: ServerData<I_old>): ServerData<I>;
-  mapClientData(input: ClientData<I_old>): ClientData<I>;
+class Middleware<I_old: {}, I: {}, Payload> {
+  payload: Payload;
+  constructor(payload: Payload) {
+    this.payload = payload;
+  }
+  mapServerData(serverData: ServerData<I_old>): ServerData<I> {
+    throw "hello";
+  }
+  mapClientData(clientData: ClientData<I_old>): ClientData<I> {
+    throw "world";
+  }
 }
 
-export class Fragment<I: {}> implements Middleware<I, I> {
-  urlFragment: string;
-  constructor(urlFragment: string) {
-    this.urlFragment = urlFragment;
-  }
+export class Fragment<I: {}> extends Middleware<I, I, string> {
   mapServerData(serverData: ServerData<I>): ServerData<I> {
     return {
       ...serverData,
-      url: `${serverData.url}/${this.urlFragment}`
+      url: `${serverData.url}/${this.payload}`
     };
   }
   mapClientData(clientData: ClientData<I>): ClientData<I> {
     return {
       ...clientData,
-      url: `${clientData.url}/${this.urlFragment}`
+      url: `${clientData.url}/${this.payload}`
     };
   }
 }
@@ -38,35 +42,34 @@ export class Fragment<I: {}> implements Middleware<I, I> {
 type $Merge<A: {}, B: {}> = { ...$Exact<A>, ...$Exact<B> };
 type $ExtractTypes<O: {}> = $ObjMap<O, <V>(TypeRep<V>) => V>;
 
-export class QueryParams<I: {}, P: {}>
-  implements Middleware<I, $Merge<I, $ExtractTypes<P>>> {
-  params: P;
-  constructor(params: P) {
-    this.params = params;
-  }
+export class QueryParams<I: {}, P: {}> extends Middleware<
+  I,
+  $Merge<I, $ExtractTypes<P>>,
+  P
+> {
   mapServerData(serverData: ServerData<I>): ServerData<I> {
     return {
       ...serverData,
-      queryParams: { ...serverData.queryParams, ...this.params }
+      queryParams: { ...serverData.queryParams, ...this.payload }
     };
   }
   mapClientData(clientData: ClientData<I>): ClientData<I> {
     return {
       ...clientData,
-      queryParams: { ...clientData.queryParams, ...this.params }
+      queryParams: { ...clientData.queryParams, ...this.payload }
     };
   }
 }
 
 export interface Endpoint<I: {}, O> {
-  append<I_new: {}>(middleware: Middleware<I, I_new>): Endpoint<I_new, O>;
+  append<I_new: {}>(middleware: Middleware<I, I_new, *>): Endpoint<I_new, O>;
   fragment(urlFragment: string): Endpoint<I, O>;
   queryParams<P: {}>(params: P): Endpoint<$Merge<I, $ExtractTypes<P>>, O>;
 }
 
 export class EndpointImpl<I: {}, O> implements Endpoint<I, O> {
   constructor() {}
-  append<I_new: {}>(middleware: Middleware<I, I_new>): Endpoint<I_new, O> {
+  append<I_new: {}>(middleware: Middleware<I, I_new, *>): Endpoint<I_new, O> {
     return new Snoc({ previous: this, middleware });
   }
 
@@ -81,7 +84,7 @@ export class EndpointImpl<I: {}, O> implements Endpoint<I, O> {
 
 type SnocData<I_old: {}, O_old, I: {}> = {
   previous: Endpoint<I_old, O_old>,
-  middleware: Middleware<I_old, I>
+  middleware: Middleware<I_old, I, *>
 };
 
 export class Snoc<I_old: {}, O_old, I: {}, O> extends EndpointImpl<I, O> {
