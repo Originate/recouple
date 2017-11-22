a4b95de9db403cb533e7465f2c339c964c318398
Sean Lee <freshdried@gmail.com>
2017-11-22 15:47:39 -0500

    more tests

--- src/index.test.js
+++ src/index.test.js
@@ -7,10 +7,55 @@ it.only("works", () => {
     .use({ queryParams: SafeAPI.queryParams })
     .use({ fragment: SafeAPI.fragment });
 
-  // $FlowFixMe
-  const myEndpoint: SafeAPI.Endpoint<*, {}, string> = endpoint()
+  const myEndpoint: SafeAPI.Endpoint<*, { foo: string}, string> = endpoint()
     .fragment("hello")
     .queryParams({ foo: t.string })
 
   console.log(JSON.stringify(myEndpoint, null, 2));
 });
+
+// type level tests
+
+// happy path
+() => {
+  const endpoint = SafeAPI.safeAPI()
+    .use({ queryParams: SafeAPI.queryParams })
+    .use({ fragment: SafeAPI.fragment });
+
+  // ok
+  const myEndpoint: SafeAPI.Endpoint<*, { foo: string }, string> = endpoint()
+    .fragment("hello")
+    .queryParams({ foo: t.string })
+}
+
+// error when using a nonexistent middleware
+() => {
+  const endpoint = SafeAPI.safeAPI();
+
+  const myEndpoint: SafeAPI.Endpoint<*, {}, string> = endpoint()
+    // $FlowFixMe
+    .fragment("hello")
+}
+
+// error with invalid input type annotation
+() => {
+  const endpoint = SafeAPI.safeAPI()
+    .use({ queryParams: SafeAPI.queryParams })
+    .use({ fragment: SafeAPI.fragment });
+
+  // $FlowFixMe
+  const myEndpoint: SafeAPI.Endpoint<*, {}, string> = endpoint()
+    .fragment("hello")
+    .queryParams({ foo: t.string })
+}
+
+// error with invalid input type annotation
+() => {
+  const endpoint = SafeAPI.safeAPI()
+    .use({ queryParams: SafeAPI.queryParams })
+    .use({ fragment: SafeAPI.fragment });
+
+  // $FlowFixMe
+  const myEndpoint: SafeAPI.Endpoint<*, { foo: string }, string> = endpoint()
+    .fragment("hello")
+}
