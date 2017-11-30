a08915a7989ec086c4c5460332e587e36e908320
sean lee <freshdried@gmail.com>
2017-11-30 11:15:17 -0500

    Switch `example/backend/main.js` and `example/backend/index.js` (#35)
    * Switch `example/backend/main.js` and `example/backend/index.js`
    
    * wip

--- example/backend/index.js
+++ example/backend/index.js
@@ -1,2 +1,15 @@
-require("babel-register");
-require("./main").main();
+// @flow
+import * as SafeKoa from "safe-api-koa";
+import * as API from "example-shared/api";
+import Koa from "koa";
+
+export function main() {
+  const app = new Koa();
+  app.use(
+    SafeKoa.safeGet(API.hello, async () => {
+      return "world";
+    })
+  );
+  app.listen(8080);
+  console.log("backend started on port 8080");
+}
