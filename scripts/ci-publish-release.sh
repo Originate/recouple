f16981330078fa5a8f814d953513cf95d6ff8a72
Sean Lee <freshdried@gmail.com>
2017-12-02 23:31:52 -0500

    release on ci

---
+++ scripts/ci-publish-release.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+./node_modules/.bin/lerna publish --skip-git --yes
+git ls-files | grep '\package.json$' | xargs git add
+VERSION=$(cd recouple; yarn info --json | jq '.data.version' --raw-output)
+git commit  -m "[ci skip] version bump: $VERSION"
+git push origin HEAD
